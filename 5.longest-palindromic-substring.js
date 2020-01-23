f99b7f71360f584136187b1db7dc94c41f930dca
SASUKE40 <sasuke688848@gmail.com>
2020-01-23 20:49:45 +0800

    chore: add 5.longest-palindromic-substring

---
+++ 5.longest-palindromic-substring.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=5 lang=javascript
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    
+};
+// @lc code=end
+
