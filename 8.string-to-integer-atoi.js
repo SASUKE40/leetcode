42f0f65be79238fa90955a348c12077d78753e07
SASUKE40 <sasuke688848@gmail.com>
2020-01-26 23:36:11 +0800

    chore: add 8.string-to-integer-atoi

---
+++ 8.string-to-integer-atoi.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=8 lang=javascript
+ *
+ * [8] String to Integer (atoi)
+ */
+
+// @lc code=start
+/**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function(str) {
+    
+};
+// @lc code=end
+
