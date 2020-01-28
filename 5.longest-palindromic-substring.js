da1399d762f2f7738d9719f5a44c9963350914e7
SASUKE40 <sasuke688848@gmail.com>
2020-01-28 15:26:04 +0800

    chore: add 5.longest-palindromic-substring

--- 5.longest-palindromic-substring.js
+++ 5.longest-palindromic-substring.js
@@ -10,7 +10,31 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    
+  if (s === null || s.length === 0) {
+    return '';
+  }
+  let start = 0,
+    end = 0;
+  for (let i = 0; i < s.length; i++) {
+    let len1 = expandAroundCenter(s, i, i);
+    let len2 = expandAroundCenter(s, i, i + 1);
+    let len = Math.max(len1, len2);
+    if (len > end - start) {
+      start = Math.ceil(i - (len - 1) / 2);
+      end = start + len;
+    }
+  }
+  return s.substring(start, end);
+};
+
+var expandAroundCenter = function(s, left, right) {
+  let L = left,
+    R = right;
+  while (L >= 0 && R < s.length && s[L] === s[R]) {
+    L--;
+    R++;
+  }
+  return R - L - 1;
 };
 // @lc code=end
-
+console.log(longestPalindrome('babad'));
