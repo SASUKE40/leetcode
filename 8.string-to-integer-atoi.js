558f794ac0c0486838f533cd4a096f241aeccca4
SASUKE40 <sasuke688848@gmail.com>
2020-01-28 15:54:52 +0800

    chore: add 8.string-to-integer-atoi

--- 8.string-to-integer-atoi.js
+++ 8.string-to-integer-atoi.js
@@ -11,5 +11,15 @@
  */
 var myAtoi = function(str) {
   let trimmed = str.trim();
+  let parsed = parseInt(trimmed, 10);
+  if (isNaN(parsed)) {
+    return 0;
+  } else if (parsed >= Math.pow(2, 31)) {
+    return Math.pow(2, 31) - 1;
+  } else if (parsed <= Math.pow(-2, 31)) {
+    return Math.pow(-2, 31);
+  } else {
+    return parsed;
+  }
 };
 // @lc code=end
