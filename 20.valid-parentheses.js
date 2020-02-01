2eb116f64f4c4112512a76f5f34d17467208afea
SASUKE40 <sasuke688848@gmail.com>
2020-02-01 23:42:21 +0800

    chore: add 20.valid-parentheses

---
+++ 20.valid-parentheses.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=20 lang=javascript
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+};
+// @lc code=end
+
