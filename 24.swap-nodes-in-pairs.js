0ac28f65c4d3196dea0918f6f64ab51ee529097c
SASUKE40 <sasuke688848@gmail.com>
2020-02-06 22:05:29 +0800

    chore: add 24.swap-nodes-in-pairs

---
+++ 24.swap-nodes-in-pairs.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=24 lang=javascript
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    
+};
+// @lc code=end
+
