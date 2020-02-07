5d49060c0c37def6c6608f03d35a2ff3188e9603
SASUKE40 <sasuke688848@gmail.com>
2020-02-07 17:48:04 +0800

    chore: add 15.3-sum

--- 15.3-sum.js
+++ 15.3-sum.js
@@ -10,7 +10,29 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    
+  let ans = [];
+  nums = nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+      let high = nums.length - 1;
+      let low = i + 1;
+      let sum = 0 - nums[i];
+      while (low < high) {
+        if (nums[high] + nums[low] === sum) {
+          ans.push([nums[i], nums[low], nums[high]]);
+          while (low < high && nums[high] === nums[high - 1]) high--;
+          while (low < high && nums[low] === nums[low + 1]) low++;
+          low++;
+          high--;
+        } else if (nums[high] + nums[low] > sum) {
+          high--;
+        } else if (nums[high] + nums[low] < sum) {
+          low++;
+        }
+      }
+    }
+  }
+  return ans;
 };
 // @lc code=end
-
