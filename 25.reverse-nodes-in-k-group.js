2fb593e62e18d6ebd7f7ffda951cc788e01aa17d
SASUKE40 <sasuke688848@gmail.com>
2020-02-07 17:48:18 +0800

    chore: add 25.reverse-nodes-in-k-group

---
+++ 25.reverse-nodes-in-k-group.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=25 lang=javascript
+ *
+ * [25] Reverse Nodes in k-Group
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function(head, k) {
+    
+};
+// @lc code=end
+
