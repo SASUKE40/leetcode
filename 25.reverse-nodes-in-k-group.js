05fe75905aa824c531ad1c9498908b626a2194e8
SASUKE40 <sasuke688848@gmail.com>
2020-02-08 22:35:10 +0800

    chore: add 25.reverse-nodes-in-k-group

--- 25.reverse-nodes-in-k-group.js
+++ 25.reverse-nodes-in-k-group.js
@@ -18,7 +18,27 @@
  * @return {ListNode}
  */
 var reverseKGroup = function(head, k) {
-    
+  if (!head) return null;
+  let tail = head;
+  for (let i = 1; i < k; i++) {
+    tail = tail.next;
+    if (!tail) return head;
+  }
+  let next = tail.next;
+  tail.next = null;
+  reverse(head);
+  head.next = reverseKGroup(next, k);
+  return tail;
 };
-// @lc code=end
 
+function reverse(curr) {
+  let prev = null;
+  while (curr) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
+// @lc code=end
