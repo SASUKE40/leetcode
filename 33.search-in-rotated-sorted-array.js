2a6d44dd422bb045beff986cacf06e926a89bcd5
SASUKE40 <sasuke688848@gmail.com>
2020-02-09 17:34:29 +0800

    chore: add 33.search-in-rotated-sorted-array

---
+++ 33.search-in-rotated-sorted-array.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=33 lang=javascript
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+};
+// @lc code=end
