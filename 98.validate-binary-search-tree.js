a0979b652bc89ced291071ff3d003088a8e99508
SASUKE40 <sasuke688848@gmail.com>
2020-02-15 19:50:25 +0800

    chore: add 98.validate-binary-search-tree

---
+++ 98.validate-binary-search-tree.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=98 lang=javascript
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    
+};
+// @lc code=end
+
