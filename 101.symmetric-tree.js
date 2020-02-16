8642a642765a81646038afb7f34453cdbe476b61
SASUKE40 <sasuke688848@gmail.com>
2020-02-16 19:30:55 +0800

    chore: add 101.symmetric-tree

---
+++ 101.symmetric-tree.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=101 lang=javascript
+ *
+ * [101] Symmetric Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    
+};
+// @lc code=end
+
