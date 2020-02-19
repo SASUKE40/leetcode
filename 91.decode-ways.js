80bc7b076e853f8130d94d6bc7251a7ab0f0bf34
SASUKE40 <sasuke688848@gmail.com>
2020-02-19 19:34:37 +0800

    chore: add 91.decode-ways

---
+++ 91.decode-ways.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=91 lang=javascript
+ *
+ * [91] Decode Ways
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    
+};
+// @lc code=end
+
