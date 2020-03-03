0770bcf71ff73f975668e26b0ba0bbba94f2fc7e
SASUKE40 <sasuke688848@gmail.com>
2020-03-03 18:14:01 +0800

    chore: add 91.decode-ways

--- 91.decode-ways.js
+++ 91.decode-ways.js
@@ -10,7 +10,22 @@
  * @return {number}
  */
 var numDecodings = function(s) {
-    
+  if (s === null || s.length === 0) {
+    return 0;
+  }
+  let dp = new Array(s.length + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = s[0] === '0' ? 0 : 1;
+  for (let i = 2; i < s.length + 1; i++) {
+    let one = Number(s.slice(i - 1, i));
+    let two = Number(s.slice(i - 2, i));
+    if (two >= 10 && two <= 26) {
+      dp[i] = dp[i - 2];
+    }
+    if (one >= 1 && one <= 9) {
+      dp[i] += dp[i - 1];
+    }
+  }
+  return dp.pop();
 };
 // @lc code=end
-
