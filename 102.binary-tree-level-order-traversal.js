17fd66b133efafa75527490cbe959caacc7093ee
SASUKE40 <sasuke688848@gmail.com>
2020-03-04 17:45:47 +0800

    chore: add 102.binary-tree-level-order-traversal

--- 102.binary-tree-level-order-traversal.js
+++ 102.binary-tree-level-order-traversal.js
@@ -17,7 +17,18 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    
+  let result = [];
+  let level = 0;
+  helper(root, result, level);
+  return result;
 };
-// @lc code=end
 
+function helper(root, list, level) {
+  if (root === null) return;
+  if (!list[level]) list[level] = [];
+  list[level].push(root.val);
+  helper(root.left, list, level + 1);
+  helper(root.right, list, level + 1);
+}
+
+// @lc code=end
