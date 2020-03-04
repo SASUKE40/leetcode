3c8651ffaf603bf3d32b867a6b65c630583e6d6f
SASUKE40 <sasuke688848@gmail.com>
2020-03-04 18:27:39 +0800

    chore: add 103.binary-tree-zigzag-level-order-traversal

--- 103.binary-tree-zigzag-level-order-traversal.js
+++ 103.binary-tree-zigzag-level-order-traversal.js
@@ -17,7 +17,20 @@
  * @return {number[][]}
  */
 var zigzagLevelOrder = function(root) {
-    
+  let list = [];
+  traverse(root, list, 0);
+  return list;
 };
-// @lc code=end
 
+function traverse(root, list, level) {
+  if (root === null) return;
+  if (!list[level]) list[level] = [];
+  if (level % 2 === 1) {
+    list[level].unshift(root.val);
+  } else {
+    list[level].push(root.val);
+  }
+  traverse(root.left, list, level + 1);
+  traverse(root.right, list, level + 1);
+}
+// @lc code=end
