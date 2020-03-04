f84ec5796e2ac325c8783d31b09d48ae012a62f2
SASUKE40 <sasuke688848@gmail.com>
2020-03-04 17:14:24 +0800

    chore: add 101.symmetric-tree

--- 101.symmetric-tree.js
+++ 101.symmetric-tree.js
@@ -17,7 +17,16 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    
+  return isMirror(root, root);
 };
-// @lc code=end
 
+function isMirror(left, right) {
+  if (left === null && right === null) return true;
+  if (left === null || right === null) return false;
+  return (
+    left.val === right.val &&
+    isMirror(left.left, right.right) &&
+    isMirror(left.right, right.left)
+  );
+}
+// @lc code=end
