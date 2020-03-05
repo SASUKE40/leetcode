a5d4d8f899cf008d49d4862963821a290b173201
SASUKE40 <sasuke688848@gmail.com>
2020-03-05 15:10:19 +0800

    chore: add 168.excel-sheet-column-title

--- 168.excel-sheet-column-title.js
+++ 168.excel-sheet-column-title.js
@@ -9,17 +9,23 @@
  * @param {number} n
  * @return {string}
  */
+// var convertToTitle = function(n) {
+//   let s = '';
+//   function helper(n) {
+//     if (n === 0) return;
+//     let curChar = (n - 1) % 26;
+//     s = String.fromCharCode(65 + curChar) + s;
+//     let nextN = Math.floor((n - 1) / 26);
+//     helper(nextN);
+//   }
+//   helper(n);
+//   return s;
+// };
+
 var convertToTitle = function(n) {
-  let s = '';
-  function helper(n) {
-    if (n === 0) return;
-    let curChar = (n - 1) % 26;
-    s = String.fromCharCode(65 + curChar) + s;
-    let nextN = Math.floor((n - 1) / 26);
-    helper(nextN);
-  }
-  helper(n);
-  return s;
+  return n === 0
+    ? ''
+    : convertToTitle(Math.floor(--n / 26)) + String.fromCharCode(65 + (n % 26));
 };
 
 // @lc code=end
