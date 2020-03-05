f63faa6b36ec90aad9d27a7848b8460f0e6bedfa
SASUKE40 <sasuke688848@gmail.com>
2020-03-05 17:29:18 +0800

    chore: add 189.rotate-array

---
+++ 189.rotate-array.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=189 lang=javascript
+ *
+ * [189] Rotate Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  k = k % nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+};
+
+function reverse(nums, l, r) {
+  while (l < r) {
+    let temp = nums[l];
+    nums[l] = nums[r];
+    nums[r] = temp;
+    l++;
+    r--;
+  }
+}
+
+// @lc code=end
