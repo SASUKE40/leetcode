a6cda42542fab107bcc9e1ee08a939ddd8a9b2f9
SASUKE40 <sasuke688848@gmail.com>
2020-03-06 22:21:24 +0800

    chore: add 218.the-skyline-problem

---
+++ 218.the-skyline-problem.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=218 lang=javascript
+ *
+ * [218] The Skyline Problem
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} buildings
+ * @return {number[][]}
+ */
+var getSkyline = function(buildings) {
+    
+};
+// @lc code=end
+
