021c6d991e7fce64930abe8d405948f5613321bd
SASUKE40 <sasuke688848@gmail.com>
2020-03-06 23:32:48 +0800

    chore: add 237.delete-node-in-a-linked-list

---
+++ 237.delete-node-in-a-linked-list.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=237 lang=javascript
+ *
+ * [237] Delete Node in a Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+  node.val = node.next.val;
+  node.next = node.next.next;
+};
+// @lc code=end
