099b7aa40cf69efbac1eb879092f626a597cea9d
SASUKE40 <sasuke688848@gmail.com>
2020-03-09 00:28:39 +0800

    chore: add 2.add-two-numbers

---
+++ 2.add-two-numbers.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=2 lang=javascript
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    
+};
+// @lc code=end
+
