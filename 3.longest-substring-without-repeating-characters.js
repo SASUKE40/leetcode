80eb27ec763219537ccc04e52fef876f52f10e15
SASUKE40 <sasuke688848@gmail.com>
2020-03-13 22:40:50 +0800

    chore: add 3.longest-substring-without-repeating-characters

---
+++ 3.longest-substring-without-repeating-characters.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=3 lang=javascript
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  let ans = 0;
+  let map = new Map();
+  for (let i = 0, j = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      j = Math.max(map.get(s[i]) || 0, j);
+    }
+    ans = Math.max(ans, i - j + 1);
+    map.set(s[i], i + 1);
+  }3.longest-substring-without-repeating-characters
+  return ans;
+};
+// @lc code=end
