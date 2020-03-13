709bb8ac5c2fa9cfad5613f75940501f232c1c62
SASUKE40 <sasuke688848@gmail.com>
2020-03-13 16:01:06 +0800

    chore: add 218.the-skyline-problem

--- 218.the-skyline-problem.js
+++ 218.the-skyline-problem.js
@@ -10,7 +10,28 @@
  * @return {number[][]}
  */
 var getSkyline = function(buildings) {
-    
+  let set = new Set();
+  for (let [l, r, h] of buildings) {
+    set.add(l);
+    set.add(r);
+  }
+  let points = [...set].sort((a, b) => a - b);
+  let n = buildings.length;
+  let res = [[-1, 0]];
+  for (let p of points) {
+    let i = 0;
+    let height = 0;
+    while (i < n && buildings[i][0] <= p) {
+      if (buildings[i][1] > p) {
+        height = Math.max(height, buildings[i][2]);
+      }
+      i++;
+    }
+    if (height === res[res.length - 1][1]) {
+      continue;
+    }
+    res.push([p, height]);
+  }
+  return res.slice(1);
 };
 // @lc code=end
-
