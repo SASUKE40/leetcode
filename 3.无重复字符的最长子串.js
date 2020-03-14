771793682d743b67776e9656da4a4b3e1aa46949
SASUKE40 <sasuke688848@gmail.com>
2020-03-14 16:38:41 +0800

    chore: add 3.无重复字符的最长子串

---
+++ 3.无重复字符的最长子串.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=javascript
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  let ans = 0;
+  let map = new Map();
+  for (let i = 0, j = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      j = Math.max(map.get(s[i]) || 0, j);
+    }
+    ans = Math.max(ans, i - j + 1);
+    map.set(s[i], i + 1);
+  }
+  return ans;
+};
+// @lc code=end
+
