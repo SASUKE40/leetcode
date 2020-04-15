d65cda712fa264a94034405c2d3e6a940712761e
Edward Elric <SASUKE688848@gmail.com>
2020-04-15 21:16:47 +0800

    chore: add 23.合并k个排序链表

---
+++ 23.合并k个排序链表.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=javascript
+ *
+ * [23] 合并K个排序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {
+  if (lists === null || lists.length === 0) return null;
+  let res = lists[0];
+  for (let i = 1; i < lists.length; i++) {
+    res = mergeTwoLists(res, lists[i]);
+  }
+  return res;
+};
+
+function mergeTwoLists(l1, l2) {
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l2.next, l1);
+    return l2;
+  }
+}
+// @lc code=end
