f7d2b11c25d4861c30998370691cf3817a74f75f
Edward Elric <sasuke688848@gmail.com>
2020-05-02 16:58:04 +0800

    chore: add 4.寻找两个有序数组的中位数

--- 4.寻找两个有序数组的中位数.js
+++ 4.寻找两个有序数组的中位数.js
@@ -10,8 +10,47 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
-
+var findMedianSortedArrays = function (nums1, nums2) {
+  const n = nums1.length;
+  const m = nums2.length;
+  const left = ((n + m + 1) / 2) >> 0;
+  const right = ((n + m + 2) / 2) >> 0;
+  return (
+    (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) +
+      getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) *
+    0.5
+  );
 };
-// @lc code=end
 
+function getKth(nums1, start1, end1, nums2, start2, end2, k) {
+  const len1 = end1 - start1 + 1;
+  const len2 = end2 - start2 + 1;
+  if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+  if (len1 === 0) return nums2[start2 + k - 1];
+  if (k === 1) return Math.min(nums1[start1], nums2[start2]);
+  const i = start1 + Math.min(len1, (k / 2) >> 0) - 1;
+  const j = start2 + Math.min(len2, (k / 2) >> 0) - 1;
+  if (nums1[i] > nums2[j]) {
+    return getKth(
+      nums1,
+      start1,
+      end1,
+      nums2,
+      j + 1,
+      end2,
+      k - (j - start2 + 1)
+    );
+  } else {
+    return getKth(
+      nums1,
+      i + 1,
+      end1,
+      nums2,
+      start2,
+      end2,
+      k - (i - start1 + 1)
+    );
+  }
+}
+
+// @lc code=end
