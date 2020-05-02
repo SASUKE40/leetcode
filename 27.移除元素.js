7fe32a458f84be4c87249c8647e0ecb171e606f1
Edward Elric <sasuke688848@gmail.com>
2020-05-02 19:58:28 +0800

    chore: add 27.移除元素

---
+++ 27.移除元素.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=javascript
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+  return j;
+};
+// @lc code=end
