bdd4a07633e6aece1b7429ddb035abeba9cdee6e
Edward Elric <sasuke688848@gmail.com>
2020-05-07 22:43:54 +0800

    chore: add 64.最小路径和

---
+++ 64.最小路径和.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=64 lang=javascript
+ *
+ * [64] 最小路径和
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function (grid) {
+  let m = grid.length;
+  let n = grid[0].length;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === 0 && j === 0) continue;
+      if (i === 0) grid[i][j] += grid[i][j - 1];
+      else if (j === 0) grid[i][j] += grid[i - 1][j];
+      else grid[i][j] = grid[i][j] + Math.min(grid[i - 1][j], grid[i][j - 1]);
+    }
+  }
+  return grid[m - 1][n - 1];
+};
+// @lc code=end
