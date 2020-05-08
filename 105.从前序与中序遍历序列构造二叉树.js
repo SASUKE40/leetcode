44ed481a2d5a5276c59f8c2d7d88287764565631
Edward Elric <sasuke688848@gmail.com>
2020-05-08 17:16:31 +0800

    chore: add 105.从前序与中序遍历序列构造二叉树

---
+++ 105.从前序与中序遍历序列构造二叉树.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=105 lang=javascript
+ *
+ * [105] 从前序与中序遍历序列构造二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+  if (inorder.length === 0) return null;
+  let root = new TreeNode(preorder[0]);
+  let mid = inorder.indexOf(preorder[0]);
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+  return root;
+};
+// @lc code=end
