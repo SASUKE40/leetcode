30b4c288135dff9117aff83d52a2a95f5ccfbedf
Edward Elric <sasuke688848@gmail.com>
2020-05-08 17:46:35 +0800

    chore: add 139.单词拆分

---
+++ 139.单词拆分.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=139 lang=javascript
+ *
+ * [139] 单词拆分
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function (s, wordDict) {
+  let dp = new Array(s.length + 1).fill(false);
+  dp[0] = true;
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dp[j] && wordDict.includes(s.substring(j, i))) {
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+  return dp[s.length];
+};
+// @lc code=end
