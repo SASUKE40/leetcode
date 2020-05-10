cc8a9926e00ee3ef4aefdf2f0ecfcf45818b7cd3
Edward Elric <SASUKE688848@gmail.com>
2020-05-10 16:51:43 +0800

    chore: add 198.打家劫舍

---
+++ 198.打家劫舍.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=javascript
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+  let dp = new Array(nums.length + 1);
+  dp[0] = 0;
+  dp[1] = nums[0];
+  for (let i = 2; i <= nums.length; i++) {
+    dp[i] = Math.max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+  }
+  return dp[nums.length] || 0;
+};
+// @lc code=end
