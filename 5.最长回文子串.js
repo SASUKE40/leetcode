06a25d92affd0fefbee168cb653c06c1e685ed6b
Edward Elric <sasuke688848@gmail.com>
2020-05-21 22:24:44 +0800

    chore: add 5.最长回文子串

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -9,8 +9,22 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {
-
+var longestPalindrome = function (s) {
+  let n = s.length;
+  let dp = new Array(n);
+  let ans = '';
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = new Array(n);
+  }
+  for (let l = 0; l < n; l++) {
+    for (let i = 0; i + l < n; i++) {
+      let j = i + l;
+      if (l === 0) dp[i][j] = true;
+      else if (l === 1) dp[i][j] = s[i] === s[j];
+      else dp[i][j] = dp[i + 1][j - 1] && s[i] === s[j];
+      if (dp[i][j] && j - i + 1 > ans.length) ans = s.substring(i, j + 1);
+    }
+  }
+  return ans;
 };
 // @lc code=end
-
