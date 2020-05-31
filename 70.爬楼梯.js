d64f6ee7ced71e7dd4a12033fe79652c56464fd5
Edward Elric <SASUKE688848@gmail.com>
2020-05-31 17:27:17 +0800

    chore: add 70.爬楼梯

--- 70.爬楼梯.js
+++ 70.爬楼梯.js
@@ -10,14 +10,13 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-  if (n <= 2) return n;
-  let pre1 = 1;
-  let pre2 = 2;
-  for (let i = 2; i < n; i++) {
-    let cur = pre1 + pre2;
-    pre1 = pre2;
-    pre2 = cur;
+  let pre = 1;
+  let cur = 1;
+  for (let i = 2; i <= n; i++) {
+    let temp = cur;
+    cur += pre;
+    pre = temp;
   }
-  return pre2;
+  return cur;
 };
 // @lc code=end
