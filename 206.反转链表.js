3558d7c3c31ffa545f1bcace89c41187416980d9
Edward Elric <SASUKE688848@gmail.com>
2020-06-03 22:08:38 +0800

    chore: add 206.反转链表

---
+++ 206.反转链表.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let pre = null;
+  let cur = head;
+  while (cur) {
+    let next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+  }
+  return pre;
+};
+// @lc code=end
