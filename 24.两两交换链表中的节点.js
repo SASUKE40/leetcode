10e37f39e92f269d71bfb6664d971eedd7f56a27
Edward Elric <SASUKE688848@gmail.com>
2020-06-03 22:01:19 +0800

    chore: add 24.两两交换链表中的节点

--- 24.两两交换链表中的节点.js
+++ 24.两两交换链表中的节点.js
@@ -16,8 +16,12 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var swapPairs = function(head) {
-
+var swapPairs = function (head) {
+  if (!head || !head.next) return head;
+  let firstNode = head;
+  let secondNode = head.next;
+  firstNode.next = swapPairs(secondNode.next);
+  secondNode.next = firstNode;
+  return secondNode;
 };
 // @lc code=end
-
