8fb39ea392d8d3752522ffbf18bb0ed9ae6c61d0
Edward Elric <SASUKE688848@gmail.com>
2020-06-03 21:35:34 +0800

    chore: add 167.两数之和-ii-输入有序数组

---
+++ 167.两数之和-ii-输入有序数组.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=javascript
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+  while (left < right) {
+    let sum = numbers[left] + numbers[right];
+    if (sum === target) return [left + 1, right + 1];
+    else if (sum < target) left++;
+    else right--;
+  }
+  return [-1 - 1];
+};
+// @lc code=end
