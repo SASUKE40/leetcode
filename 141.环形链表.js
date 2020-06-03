4e88a6068a09dc15e6a5e9608f55256993ff1aa9
Edward Elric <SASUKE688848@gmail.com>
2020-06-03 22:17:10 +0800

    chore: add 141.环形链表

---
+++ 141.环形链表.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=javascript
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let slow = (fast = head);
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false;
+};
+// @lc code=end
