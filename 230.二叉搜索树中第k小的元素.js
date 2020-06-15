2df70ad49dd486d9ed19b1e675e0a608dc84b0c7
Edward Elric <sasuke688848@gmail.com>
2020-06-15 13:11:22 +0800

    chore: add 230.二叉搜索树中第k小的元素

---
+++ 230.二叉搜索树中第k小的元素.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=javascript
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+  let stack = [];
+  while (true) {
+    while (root) {
+      stack.push(root);
+      root = root.left;
+    }
+    root = stack.pop();
+    k--;
+    if (!k) return root.val;
+    root = root.right;
+  }
+};
+// @lc code=end
