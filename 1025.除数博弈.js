7c74ed30ae1fa2767edd48a4454727b97742253c
Edward Elric <sasuke688848@gmail.com>
2020-07-24 14:41:56 +0800

    chore: add 1025.除数博弈

---
+++ 1025.除数博弈.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1025 lang=javascript
+ *
+ * [1025] 除数博弈
+ */
+
+// @lc code=start
+/**
+ * @param {number} N
+ * @return {boolean}
+ */
+var divisorGame = function (N) {
+  return N % 2 === 0;
+};
+// @lc code=end
