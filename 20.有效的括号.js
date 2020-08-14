e1092fe6f314f64842a4769ecb8a1f07a83f3fb8
Edward Elric <SASUKE688848@gmail.com>
2020-08-14 23:08:32 +0800

    chore: add 20.有效的括号

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -9,8 +9,17 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-
+var isValid = function (s) {
+  let stack = [];
+  let map = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  };
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]]) stack.push(s[i]);
+    else if (s[i] !== map[stack.pop()]) return false;
+  }
+  return stack.length === 0;
 };
 // @lc code=end
-
