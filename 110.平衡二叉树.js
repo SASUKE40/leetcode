18206660667b63b5958f565618446f547695e1c8
Edward Elric <SASUKE688848@gmail.com>
2020-08-17 22:57:19 +0800

    chore: add 110.平衡二叉树

---
+++ 110.平衡二叉树.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=javascript
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  if (!root) {
+    return true;
+  }
+  return (
+    Math.abs(height(root.left) - height(root.right)) <= 1 &&
+    isBalanced(root.left) &&
+    isBalanced(root.right)
+  );
+};
+
+function height(root) {
+  if (!root) {
+    return 0;
+  }
+  return Math.max(height(root.left), height(root.right)) + 1;
+}
+// @lc code=end
