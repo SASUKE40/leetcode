3495dd9a0ae358e268d7bdfef11513256106eaf1
Edward Elric <SASUKE688848@gmail.com>
2020-09-27 21:59:08 +0800

    chore: add 235.二叉搜索树的最近公共祖先

---
+++ 235.二叉搜索树的最近公共祖先.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=235 lang=javascript
+ *
+ * [235] 二叉搜索树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  if (root === null || root === p || root === q) return root;
+  let L = lowestCommonAncestor(root.left, p, q);
+  let R = lowestCommonAncestor(root.right, p, q);
+  if (!L) return R;
+  if (!R) return L;
+  return root;
+};
+// @lc code=end
