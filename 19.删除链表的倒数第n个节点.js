45a767b67d8e5dcfd23823d27fdff7a33b47affb
Edward Elric <SASUKE688848@gmail.com>
2020-10-18 12:50:03 +0800

    chore: add 19.删除链表的倒数第n个节点

---
+++ 19.删除链表的倒数第n个节点.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=javascript
+ *
+ * [19] 删除链表的倒数第N个节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  let dummy = new ListNode();
+  dummy.next = head;
+  let slow = (fast = dummy);
+  let i = 0;
+  while (i++ < n) {
+    fast = fast.next;
+  }
+  while (fast && fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  slow.next = slow.next.next;
+  return dummy.next;
+};
+// @lc code=end
