22948233367ca0c30047588b3e22e0b5c765eadf
Edward Elric <SASUKE688848@gmail.com>
2020-11-13 19:30:48 +0800

    chore: add 328.奇偶链表

---
+++ 328.奇偶链表.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=javascript
+ *
+ * [328] 奇偶链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+  if (!head) return null;
+  let odd = head;
+  let even = head.next;
+  let evenHead = head.next;
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = evenHead;
+  return head;
+};
+// @lc code=end
