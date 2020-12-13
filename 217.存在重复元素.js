3de2772bb990d7d29a650a3e6d248d4c16053e0f
Edward Elric <SASUKE688848@gmail.com>
2020-12-13 11:50:46 +0800

    chore: add 217.存在重复元素

---
+++ 217.存在重复元素.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=217 lang=javascript
+ *
+ * [217] 存在重复元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  const set = new Set();
+  for (const x of nums) {
+    if (set.has(x)) {
+      return true;
+    }
+    set.add(x);
+  }
+  return false;
+};
+// @lc code=end
+
