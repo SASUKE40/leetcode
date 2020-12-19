87c1e7f05be0c6c8b3256ef26e5dfbf335dc19ff
Edward Elric <SASUKE688848@gmail.com>
2020-12-19 13:02:04 +0800

    chore: add 48.旋转图像

---
+++ 48.旋转图像.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=javascript
+ *
+ * [48] 旋转图像
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  const n = matrix.length;
+  // 水平翻转
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    for (let j = 0; j < n; j++) {
+      [matrix[i][j], matrix[n - i - 1][j]] = [matrix[n - i - 1][j], matrix[i][j]];
+    }
+  }
+  // 主对角线翻转
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+};
+// @lc code=end
+
