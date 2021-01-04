9d5203a7a1d823dd26e101c0290125f0eb96e100
Edward Elric <SASUKE688848@gmail.com>
2021-01-04 22:16:29 +0800

    chore: add 509.斐波那契数

---
+++ 509.斐波那契数.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=javascript
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  if (n < 2) {
+    return n;
+  }
+  let p = 0, q = 0, r = 1;
+  for (let i = 2; i <= n; i++) {
+    p = q;
+    q = r;
+    r = p + q;
+  }
+  return r;
+};
+// @lc code=end
+
