7a735b11bf97f029fa404a6dd081368801ce36cc
Edward Elric <SASUKE688848@gmail.com>
2021-01-09 13:30:16 +0800

    chore: add 123.买卖股票的最佳时机-iii

---
+++ 123.买卖股票的最佳时机-iii.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=123 lang=javascript
+ *
+ * [123] 买卖股票的最佳时机 III
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  const n = prices.length;
+  let buy1 = -prices[0], buy2 = -prices[0];
+  let sell1 = 0, sell2 = 0;
+  for (let i = 1; i < n; i++) {
+    buy1 = Math.max(buy1, -prices[i]);
+    sell1 = Math.max(sell1, buy1 + prices[i]);
+    buy2 = Math.max(buy2, sell1 - prices[i]);
+    sell2 = Math.max(sell2, buy2 + prices[i]);
+  }
+  return sell2;
+};
+// @lc code=end
+
