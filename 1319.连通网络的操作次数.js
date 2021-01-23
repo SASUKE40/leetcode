0529e91c2d7cdb06d2a3a63349c284ceb28f2b35
Edward Elric <sasuke688848@gmail.com>
2021-01-23 13:48:44 +0800

    chore: add 1319.连通网络的操作次数

---
+++ 1319.连通网络的操作次数.js
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=1319 lang=javascript
+ *
+ * [1319] 连通网络的操作次数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number}
+ */
+var makeConnected = function (n, connections) {
+  if (connections.length < n - 1) {
+    return -1;
+  }
+
+  const uf = new UnionFind(n);
+  for (const conn of connections) {
+    uf.unite(conn[0], conn[1]);
+  }
+
+  return uf.setCount - 1;
+};
+
+// 并查集模板
+class UnionFind {
+  constructor(n) {
+    this.parent = new Array(n).fill(0).map((element, index) => index);
+    this.size = new Array(n).fill(1);
+    // 当前连通分量数目
+    this.setCount = n;
+  }
+
+  findset(x) {
+    if (this.parent[x] === x) {
+      return x;
+    }
+    this.parent[x] = this.findset(this.parent[x]);
+    return this.parent[x];
+  }
+
+  unite(a, b) {
+    let x = this.findset(a), y = this.findset(b);
+    if (x === y) {
+      return false;
+    }
+    if (this.size[x] < this.size[y]) {
+      [x, y] = [y, x];
+    }
+    this.parent[y] = x;
+    this.size[x] += this.size[y];
+    this.setCount -= 1;
+    return true;
+  }
+
+  connected(a, b) {
+    const x = this.findset(a), y = this.findset(b);
+    return x === y;
+  }
+}
+// @lc code=end
+
