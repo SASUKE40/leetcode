c49c8980a660e3a5922eb91fca1547de47289eb3
Edward Elric <sasuke688848@gmail.com>
2021-03-02 21:28:38 +0800

    chore: add 304.二维区域和检索-矩阵不可变

---
+++ 304.二维区域和检索-矩阵不可变.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=304 lang=javascript
+ *
+ * [304] 二维区域和检索 - 矩阵不可变
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ */
+var NumMatrix = function (matrix) {
+  const m = matrix.length;
+  if (m > 0) {
+    const n = matrix[0].length;
+    this.sums = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+    for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+        this.sums[i + 1][j + 1] = this.sums[i][j + 1] + this.sums[i + 1][j] - this.sums[i][j] + matrix[i][j];
+      }
+    }
+  }
+};
+
+/** 
+ * @param {number} row1 
+ * @param {number} col1 
+ * @param {number} row2 
+ * @param {number} col2
+ * @return {number}
+ */
+NumMatrix.prototype.sumRegion = function (row1, col1, row2, col2) {
+  return this.sums[row2 + 1][col2 + 1] - this.sums[row1][col2 + 1] - this.sums[row2 + 1][col1] + this.sums[row1][col1];
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
+// @lc code=end
+
